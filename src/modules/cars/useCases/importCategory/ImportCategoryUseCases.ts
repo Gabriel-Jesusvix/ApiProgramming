9da669fd485be91c41ab5b5f793dd666bcfdc205
Gabriel Jesus <gabrieldejesus16gs@gmail.com>
2022-07-02 03:04:13 -0300

    Finished stream and configure docs with swagger ui

--- src/modules/cars/useCases/importCategory/ImportCategoryUseCases.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCases.ts
@@ -2,17 +2,53 @@ import fs from "fs";
 import { parse as csvParse } from "csv-parse";
 import { ICategoriesRepository } from "../../repositories/ICategoriesRepository";
 
+interface IImportCategory {
+  name: string;
+  description: string;
+}
+
 class ImportCategoryUseCase {
   constructor(private categoriesRepository: ICategoriesRepository) {}
-  execute(file: Express.Multer.File): void {
-    const stream = fs.createReadStream(file.path);
 
-    const parserFile = csvParse();
-    parserFile.on("data", async (line) => {
-      console.log(line);
+  loadCategories(file: Express.Multer.File): Promise<IImportCategory[]> {
+    return new Promise((resolve, reject) => {
+      const categories: IImportCategory[] = [];
+
+      const stream = fs.createReadStream(file.path);
+
+      const parserFile = csvParse();
+      stream.pipe(parserFile);
+
+      parserFile
+        .on("data", async (line) => {
+          const [name, description] = line;
+          categories.push({
+            name,
+            description,
+          });
+        })
+        .on("end", () => {
+          resolve(categories);
+        })
+        .on("error", (error) => reject(error));
     });
-    stream.pipe(parserFile);
+  }
+
+  async execute(file: Express.Multer.File): Promise<void> {
+    const categories = await this.loadCategories(file);
+    categories.map(async (category) => {
+      const { name, description } = category;
+
+      const existCategory = this.categoriesRepository.findByName(name);
+
+      if (!existCategory) {
+        this.categoriesRepository.create({
+          name,
+          description,
+        });
+      }
+    });
   }
 }
 
-export { ImportCategoryUseCase };
+export { ImportCategoryUseCase, IImportCategory };
